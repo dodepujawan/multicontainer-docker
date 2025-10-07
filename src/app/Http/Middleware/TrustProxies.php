b35406a6a3cd32f87068b2063d044b1c80a384d7
dodepujawan <122686500+dodepujawan@users.noreply.github.com>
2025-10-07 22:48:20 +0800

    Set APP_URL ke HTTPS dan update TrustProxies

---
+++ src/app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    // Terima semua proxy
+    protected $proxies = '*';
+
+    // Gunakan header X-Forwarded-* untuk mendeteksi HTTPS
+    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO;
+
+}
--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan TrustProxies untuk semua request
+    $this->app['router']->aliasMiddleware('trustproxies', TrustProxies::class);
+
+    Route::middleware(['trustproxies'])->group(function () {
+        // Semua route Laravel kamu tetap bisa diakses
+    });
     }
 }
